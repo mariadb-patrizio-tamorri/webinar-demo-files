3e6fd13640da32213ba01b5aa4e4aa5f9e0d304d
ptamorri <patrizio.tamorri@mariadb.com>
2023-12-12 02:41:31 +0100

    commit

--- KeepSqlBusy.sh
+++ KeepSqlBusy.sh
@@ -8,6 +8,6 @@ do
     COUNTER=$[$COUNTER +1]
     mysql -h 127.0.0.1 -P 3307 -u user -ppassword mydatabase -e "insert into test (id) values($COUNTER)"	
 	mysql -h 127.0.0.1 -P 3307 -u user -ppassword mydatabase -e "select max(id) from test"	
-	mysql -h 127.0.0.1 -P 3307 -u user -ppassword mydatabase -e "select sleep(5)"	
-	sleep 5
+	mysql -h 127.0.0.1 -P 3307 -u user -ppassword mydatabase -e "select sleep(1)"	
+	sleep 1
 done
---
+++ killNode1-wait-restartNode1.sh
@@ -0,0 +1,12 @@
+mysql -h 127.0.0.1 -P 3307 -u user -ppassword  mydatabase -e "create or replace table test (id integer)"
+mysql -h 127.0.0.1 -P 3307 -u user -ppassword mydatabase -e "insert into test (id) values(1)"	
+POD_ID=$(docker ps | grep mariadb_node_one | awk -F ' ' '{print $1}')
+read -p "Press any key to resume ..."
+docker stop $POD_ID
+read -p "Press any key to resume ..."
+#mysql -h 127.0.0.1 -P 3307 -u user -ppassword  mydatabase -e "create or replace table test (id integer)"
+#mysql -h 127.0.0.1 -P 3307 -u user -ppassword mydatabase -e "insert into test (id) values($COUNTER)"	
+sleep 10
+docker start $POD_ID
+read -p "Press any key to resume ..."
+mysql -h 127.0.0.1 -P 3307 -u user -ppassword mydatabase -e "insert into test (id) values(1)"	
